ed64738ee7cea8122f6a0dfbcbd5e762193d6078
Aditya Mishra <mishraaditya12a@gmail.com>
2024-12-27 12:11:09 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyComponent from './components/Mycomponent'; // Correct import
+
+const App = () => {
+    return (
+        <div>
+            <MyComponent />
+        </div>
+    );
+};
+
+export default App;
